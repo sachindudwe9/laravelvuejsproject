ac28c275c04b75e6939455ef7b7ad3f86154420b
sachin dudwe <sachindudwe@neosoftmail.com>
2022-01-10 12:34:04 +0530

    FinalProject

---
+++ app/Http/Controllers/AdminAuthController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+
+use Illuminate\Http\Request;
+
+
+
+
+use Hash;
+use Session;
+use Auth;
+
+
+class AdminAuthController extends Controller
+{
+    public function login(){
+       return view("auth.login");
+    }
+
+    public function register(){
+       return view("auth.register");
+    }
+    public function logout(Request $request) {
+      Auth::logout();
+      return redirect('/login');
+    }
+
+    public function registerUser(Request $request){
+       // echo '<pre>';print_r($request->all());exit;
+       //echo"sachin";
+        $request->validate([
+            'name'=>'required',
+            'email'=>'required',
+            'password'=>'required',
+            'confirm_password'=>'required'
+            
+        ]);
+        $this->validate($request, [
+          'password' => 'min:6',
+          'confirm_password' => 'required_with:password|same:password|min:6'
+      ]);
+      
+        $user= new User();
+        $user->name =$request->name;
+        $user->email =$request->email;
+        $user->password =$request->password;
+        $user->confirm_password =$request->confirm_password;
+        $res=$user->save();
+        if($res)
+        {
+           return back()->with('success','You have registered successfully');
+        }
+        else
+        {
+           return back()->with('fail','Something Wrong');
+        }
+
+        
+        
+    }
+    public function authenticate(Request $request)
+    {
+       
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('admin');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+    }
+    
+}
---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+  
+use Illuminate\Support\Facades\Route;
+  
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AdminAuthController;
+  
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+  
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/admin', function () {
+    return view('admin.dashboard');
+});
+  
+Auth::routes();
+  
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+  
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UserController::class);
+    Route::resource('products', ProductController::class);
+});
+
+Route::get('/login',[AdminAuthController::class,'login']);
+Route::post('login-user',[AdminAuthController::class,'authenticate'])->name('login-user');
